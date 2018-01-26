b6d59ed805ca206fdaf78d56602d4179340e3b7d
dmred <dmredchenko@gmail.com>
2018-01-26 18:40:23 +0300

    weekMap added

--- js.js
+++ js.js
@@ -1141,7 +1141,7 @@ map
         alert('da');
         }
     else  alert('net')
-    */
+
 
 'use strict';
 
@@ -1167,3 +1167,31 @@ for(let entry of recipeMap) { // то же что и recipeMap.entries()
 }// огурцов,500 гр , и т.д., массивы по 2 значения
 
 recipeMap.forEach((value, key)=> {alert(`${key} : ${value}`)});
+*/
+
+// текущие активные пользователи
+let activeUsers = [
+    {name: "Вася"},
+    {name: "Петя"},
+    {name: "Маша"}
+];
+
+// вспомогательная информация о них,
+// которая напрямую не входит в объект юзера,
+// и потому хранится отдельно
+let weakMap = new WeakMap();
+
+weakMap.set(activeUsers[0], 1);
+weakMap.set(activeUsers[1], 2);
+weakMap.set(activeUsers[2], 3);
+//weakMap.set('Katya', 4); //Будет ошибка TypeError: "Katya" is not a non-null object
+
+alert( weakMap.get(activeUsers[0]) ); // 1
+
+activeUsers.splice(0, 1); // Вася более не активный пользователь
+
+// weakMap теперь содержит только 2 элемента
+alert(weekMap.get(activeUsers[0]));
+activeUsers.splice(0, 1); // Петя более не активный пользователь
+alert(weekMap.get(activeUsers[0]));
+// weakMap теперь содержит только 1 элемент
